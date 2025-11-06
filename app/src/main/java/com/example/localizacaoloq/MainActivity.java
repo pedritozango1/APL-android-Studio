d5d9ac133b5e560615322e9d5c2886d9eb25bccb
pedritozango1 <pedritozango1@gmail.com>
2025-11-06 12:22:58 +0000

    Progecto novo

---
+++ app/src/main/java/com/example/localizacaoloq/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.localizacaoloq;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogar;
+    private Button btnCriar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnCriar=findViewById(R.id.btnConta);
+        btnLogar=findViewById(R.id.btnLoginSistema);
+
+
+        btnCriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLoginSistema();
+            }
+        });
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCriarConta();
+            }
+        });
+    }
+    private void startCriarConta(){
+        Intent navegar=new Intent(this, CriarConta.class);
+        startActivity((navegar));
+    }
+    private void startLoginSistema(){
+        Intent navegar=new Intent(this, FormHome.class);
+        startActivity((navegar));
+    }
+}
---
+++ app/src/main/java/com/example/localizacaoloq/util/Utilizador.java
@@ -0,0 +1,22 @@
+package com.example.localizacaoloq.util;
+
+public class Utilizador {
+    private String Username;
+    private  String password;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
